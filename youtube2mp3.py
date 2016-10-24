eb9712f32105fe531fac32154b0c4af1e4045562
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-10-24 11:14:33 -0700

    Update youtube2mp3.py

--- youtube2mp3.py
+++ youtube2mp3.py
@@ -13,3 +13,5 @@ main_link = 'http://www.youtubeinmp3.com'
 mp3_download_link = 'http://www.youtubeinmp3.com/download/?video='
 mp3_download_info = 'http://www.youtubeinmp3.com/fetch/?format=JSON&bitrate=1&filesize=1&video='
 extension = '&autostart=1'
+
+# Get the link and open it
