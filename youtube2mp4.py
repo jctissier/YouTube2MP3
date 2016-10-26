a23bfca00ac4269a94fd2890060db218621e87b0
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-10-26 09:06:45 -0700

    Update youtube2mp4.py

--- youtube2mp4.py
+++ youtube2mp4.py
@@ -8,3 +8,4 @@ import time
 # Request user input
 choose = colored("Paste your link and click ENTER:\n", 'red')
 link = input(choose)
+mp4_download_link = 'http://www.youtubeinmp4.com/youtube.php?video='
