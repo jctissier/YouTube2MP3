adadf0ef2f0c603bb6288c211d3e45dc0e7bec03
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-10-28 11:06:38 -0700

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -6,12 +6,6 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 import time
 
-playlist_link = 'http://www.youtube.com'
-
-
-
-
-
-
-
+# Globals
+main_link = 'http://www.youtube.com'
 mp3_download_link = 'http://www.youtubeinmp3.com/download/?video='
