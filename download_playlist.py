a150d2c958afdd2172cc71e4a612b15c724a9de5
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-10-28 11:08:58 -0700

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -11,3 +11,6 @@ main_link = 'http://www.youtube.com'
 mp3_download_link = 'http://www.youtubeinmp3.com/download/?video='
 mp3_download_info = 'http://www.youtubeinmp3.com/fetch/?format=JSON&bitrate=1&filesize=1&video='
 extension = '&autostart=1'
+
+def main():
+  
