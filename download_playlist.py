6692747be333202c43a749975f5e235ce19fd525
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-10-28 11:07:19 -0700

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -9,4 +9,4 @@ import time
 # Globals
 main_link = 'http://www.youtube.com'
 mp3_download_link = 'http://www.youtubeinmp3.com/download/?video='
-mp3_download_info = '
+mp3_download_info = 'http://www.youtubeinmp3.com/fetch/?format=JSON&bitrate=1&filesize=1&video='
