73d06a7e7c732100ebe03953d99ddbfe18d8ccac
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-11-02 11:10:18 -0700

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -53,3 +53,5 @@ def song_info(href_string, index):
     data = r.json()
     title = data['title']
     length = time.strftime("%M:%S", time.gmtime(int(data['length'])))
+    file_size = int(data['filesize']) / 1000000
+    bitrate = data['bitrate']
