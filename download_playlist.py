40820e269ac52c3a9204bc39f26055a12a275f91
Jean-Claude Tissier <jeanclaude.tca@gmail.com>
2016-11-04 10:28:01 -0700

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -74,6 +74,7 @@ def download_songs(href_string):
     soup = BeautifulSoup(r.content, "lxml")
     href = soup.find_all("a", {"id": "download"})
     href_link = href[0].get('href')
+    print(main_link + href_link)
 
 
 
